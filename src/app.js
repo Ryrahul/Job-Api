6c918ebf024345b6bca4e7328d23c3d98db60dde
Rahul <caulirahul6@gmail.com>
2023-08-11 03:09:34 +0530

    Routing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 require("../src/db/connection");
-const router = require("../src/routes/router");
+const Authrouter = require("../src/routes/authRoute");
+const Jobrouter = require("../src/routes/JobsRoute");
+const Applicantrouter = require("../src/routes/ApplicationRoute");
 const errorMiddlewar = require("./middleware/errormiddleware");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
-app.use("/api", router);
+app.use("/api/user", Authrouter);
+app.use("api/", Jobrouter);
+app.use("api/User", Applicantrouter);
 app.use(errorMiddlewar);
 
 app.listen(port, () => {
